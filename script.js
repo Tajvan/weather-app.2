f41e2594217608dfcfe0022543a93d37fe7bae83
Tajvan <118295274+Tajvan@users.noreply.github.com>
2022-12-01 12:10:22 +0200

    script

---
+++ script.js
@@ -0,0 +1,161 @@
+let apiKey = `97c2f6a3b34509ac62090edc5d18d949`;
+
+function getForecast(coordinates) {
+  let apiKeyweek = `e947cb2640f1db92e6a19005bc43b435`;
+  console.log(coordinates);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKeyweek}=metric`;
+  console.log(apiUrl);
+}
+
+function weather(response) {
+  console.log(response);
+  let cityTempchange = document.querySelector(".temperature");
+  let cityHumiditychange = document.querySelector(".humidityValue");
+  let cityPressurechange = document.querySelector(".pressureValue");
+  let cityWindchange = document.querySelector(".windValue");
+  let cityNamechange = document.querySelector("h1");
+  let weathername = document.querySelector(".weathername");
+
+  celsiusTemp = Math.round(response.data.main.temp);
+  celsiUnit = Math.round(response.data.main.temp);
+  cityNamechange.innerHTML = response.data.name;
+  cityTempchange.innerHTML = Math.round(response.data.main.temp);
+  cityHumiditychange.innerHTML = Math.round(response.data.main.humidity);
+  cityPressurechange.innerHTML = Math.round(response.data.main.pressure);
+  cityWindchange.innerHTML = response.data.wind.speed;
+  weathername.innerHTML = response.data.weather[0].description;
+
+  getForecast(response.data.coord);
+
+  function changeImg() {
+    if (response.data.weather[0].main === "Clouds") {
+      document.getElementById(
+        "boarder"
+      ).style.backgroundImage = `url("https://media.giphy.com/media/GFXNdR1tuMopi/giphy.gif")`;
+    }
+    if (
+      response.data.weather[0].main === "Rain" &&
+      response.data.weather[0].main === "Drizzle"
+    ) {
+      document.getElementById(
+        "boarder"
+      ).style.backgroundImage = `url("https://media.giphy.com/media/t7Qb8655Z1VfBGr5XB/giphy.gif")`;
+    }
+    if (response.data.weather[0].main === "Snow") {
+      document.getElementById(
+        "boarder"
+      ).style.backgroundImage = `url("https://media.giphy.com/media/RWJPtq90qOA4E/giphy.gif")`;
+    }
+    if (response.data.weather[0].main === "Thunderstorm") {
+      document.getElementById(
+        "boarder"
+      ).style.backgroundImage = `url("https://media.giphy.com/media/8xY1YYpEZ4dws/giphy.gif")`;
+    }
+    if (response.data.weather[0].main === "Clear") {
+      document.getElementById(
+        "boarder"
+      ).style.backgroundImage = `url("https://media.giphy.com/media/XBVHoZahgO0sskTGxa/giphy.gif")`;
+    }
+    if (
+      response.data.weather[0].main === "Mist" &&
+      response.data.weather[0].main === "Fog"
+    ) {
+      document.getElementById(
+        "boarder"
+      ).style.backgroundImage = `url("https://media.giphy.com/media/3o7TKESnlxQ2vNfBtK/giphy.gif")`;
+    }
+  }
+  changeImg();
+}
+
+function displayweather() {
+  let forecastElement = document.querySelector("#week-weather");
+  forecastHtml = "";
+  let days = ["Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `  <tr>
+              <th class="weekday">${day}</th>
+              <td class="daytemp">
+                <span class="min-temp">15°</span>-<span class="max-temp"
+                  >20°</span
+                >
+              </td>
+            </tr>
+            <th class="imgholder">
+              <img src="sun.png" alt="" />
+              <hr />
+            </th>`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+displayweather();
+
+function searchInput(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input");
+  let citychange = document.querySelector("h1");
+  citychange.innerHTML = city.value;
+  let weatherMetric = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=metric`;
+  axios.get(weatherMetric).then(weather);
+}
+let searchcity = document.querySelector("#searchbutton");
+searchcity.addEventListener("submit", searchInput);
+
+function showFarenheit(event) {
+  event.preventDefault();
+  let farenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  let tempshow = document.querySelector("#temperature");
+  tempshow.innerHTML = Math.round(farenheitTemp);
+}
+
+function showCelsi(event) {
+  event.preventDefault();
+  let tempshow = document.querySelector("#temperature");
+  tempshow.innerHTML = Math.round(celsiusTemp);
+}
+let farenheit = document.querySelector("#farenheit-unit");
+farenheit.addEventListener("click", showFarenheit);
+
+let celsi = document.querySelector("#celsia-unit");
+celsi.addEventListener("click", showCelsi);
+
+let celsiusTemp = null;
+
+let date = new Date();
+let days = date.getDay();
+let day = [
+  "Sunday",
+  "Monday",
+  "Tuesay",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let hours = date.getHours();
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let today = document.querySelector("#today-day");
+let time = document.querySelector("#today-time");
+let todaydate = `${day[days]}`;
+let todaytime = `${hours}:${minutes}`;
+
+today.innerHTML = todaydate;
+time.innerHTML = todaytime;
+
+function myPosition(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let apiPosition = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+  axios.get(apiPosition).then(weather);
+}
+function currentposition() {
+  navigator.geolocation.getCurrentPosition(myPosition);
+}
+let gps = document.querySelector(".gpsButton");
+gps.addEventListener("click", currentposition);
